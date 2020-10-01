bf688247547be48fda8d64e19a8db312a3dfb0b8
KuhuChawla <72125043+KuhuChawla@users.noreply.github.com>
2020-10-01 14:02:47 +0530

    Create Sum.py

---
+++ Sum.py
@@ -0,0 +1,16 @@
+import urllib.request as ur
+import json
+
+jsonurl=input("Enter url-")
+print("Retrieving",jsonurl)
+data=ur.urlopen(jsonurl).read().decode()
+print("Retrieved",len(data),"characters")
+jsonobject=json.loads(data)
+
+sum=0
+totalnumber=0
+for comment in jsonobject["comments"]:
+    sum=sum+int(comment["count"])
+    totalnumber=totalnumber+1
+print("Count:",totalnumber)
+print("Sum:",sum)
